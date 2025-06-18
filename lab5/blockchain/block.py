bcc356d53fc3cc0947bffa88baa274f32664d282
Luan31-10 <vluan9087@gmail.com>
2025-06-18 09:20:44 +0700

    add blockchain

--- lab5/blockchain/block.py
+++ lab5/blockchain/block.py
@@ -3,6 +3,7 @@ import time
 
 class Block:
     def __init__(self, index, previous_hash, timestamp, transactions, proof):
+
         self.index = index
         self.previous_hash = previous_hash
         self.timestamp = timestamp
--- lab5/blockchain/blockchain.py
+++ lab5/blockchain/blockchain.py
@@ -6,8 +6,9 @@ class Blockchain:
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        self.create_block(proof=1, previous_hash='0') # Genesis block
 
-    def create_block(self, proof, previous_hash='0'):
+    def create_block(self, proof, previous_hash):
         block = Block(len(self.chain) + 1, previous_hash, time.time(), self.current_transactions, proof)
         self.current_transactions = []
         self.chain.append(block)
@@ -20,8 +21,9 @@ class Blockchain:
         new_proof = 1
         check_proof = False
         while not check_proof:
+          
             hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
-            if hash_operation[:4] == '0000':
+            if hash_operation[:4] == '0000': 
                 check_proof = True
             else:
                 new_proof += 1
@@ -38,11 +40,13 @@ class Blockchain:
             block = chain[block_index]
             if block.previous_hash != previous_block.hash:
                 return False
+
             previous_proof = previous_block.proof
             proof = block.proof
             hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
-            if hash_operation[:4] != '0000':
+            if hash_operation[:4] != '0000': 
                 return False
+            
             previous_block = block
             block_index += 1
         return True
